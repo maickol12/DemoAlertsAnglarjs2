2de40350fba907972f1bd7ebde0905286baba887
FlavioBaez <flaviocesar_baez@hotmail.com>
2017-08-04 03:23:37 -0500

    upgrade

--- src/app/cuerpo-alerts/cuerpo-alerts.component.ts
+++ src/app/cuerpo-alerts/cuerpo-alerts.component.ts
@@ -12,14 +12,14 @@ export class CuerpoAlertsComponent implements OnInit {
 
   ngOnInit() {
     this.alertList = [
-      new CascaronAlert('camara 1', 'tipo 1', '31/07/2017'),
-      new CascaronAlert('camara 2', 'tipo 2', '31/07/2017'),
-      new CascaronAlert('camara 3', 'tipo 3', '31/07/2017'),
-      new CascaronAlert('camara 3', 'tipo 3', '31/07/2017'),
-      new CascaronAlert('camara 3', 'tipo 3', '31/07/2017'),
-      new CascaronAlert('camara 3', 'tipo 3', '31/07/2017'),
-      new CascaronAlert('camara 3', 'tipo 3', '31/07/2017'),
-      new CascaronAlert('camara 3', 'tipo 3', '31/07/2017')
+      new CascaronAlert('Borregos', 'tipo 1', '31/07/2017'),
+      new CascaronAlert('Starbucks', 'tipo 2', '31/07/2017'),
+      new CascaronAlert('Piedritas 1', 'tipo 3', '31/07/2017'),
+      new CascaronAlert('Starbucks', 'tipo 3', '31/07/2017'),
+      new CascaronAlert('Starbucks', 'tipo 3', '31/07/2017'),
+      new CascaronAlert('Starbucks', 'tipo 3', '31/07/2017'),
+      new CascaronAlert('Starbucks', 'tipo 3', '31/07/2017'),
+      new CascaronAlert('Starbucks', 'tipo 3', '31/07/2017')
 
 
     ];
